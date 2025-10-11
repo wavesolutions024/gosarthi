bef992342d48d9e214c8c5996a2c5df6327f9555
Ketan Dudka <ketandudka566123@gmail.com>
2025-10-11 18:33:35 +0530

    push

--- src/comp/herosection/Herosection.jsx
+++ src/comp/herosection/Herosection.jsx
@@ -4,12 +4,12 @@ import hro_img from "../../assets/hero.png"
 const Herosection = () => {
   return (
     <>
-      <div class="herosection_parent parent">
+      <div class="herosection_parent bg-img-cover parent"  >
         <div class="herosection_cont cont">
           <div class="left">
             <h4 className="header_text">Plan your trip now</h4>
             <h1 className="hero_heading" >Explore the world with comfortable car</h1>
-            <p>
+            <p className="hero_para" >
               Embark on unforgettable adventures and discover the world in
               unparalleled comfort and style with our fleet of exceptionally
               comfortable cars.
@@ -22,7 +22,7 @@ const Herosection = () => {
           </div>
         </div>
 
-        <div class="mask_image bg-img-cover"></div>
+        {/* <div class="mask_image bg-img-cover"></div> */}
       </div>
     </>
   );
