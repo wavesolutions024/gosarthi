c8293c83e5b276729d7f1d06ec7f7a6e5aca6935
rishabhkhade <rishabhkhade95@gmail.com>
2025-10-12 01:15:52 +0530

    pp

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Facilities from './comp/facilities/Facilities'
 import About from './comp/about/About'
 import Footer from './comp/footer/Footer'
 import BeforeFooter from './comp/before-footer/BeforeFooter'
+import { FaWhatsapp } from "react-icons/fa";
 
 function App() {
 
@@ -18,6 +19,10 @@ function App() {
         <Facilities />
         <BeforeFooter />
         <Footer />
+
+        <div className="whats_app">
+          <span><FaWhatsapp /></span>
+        </div>
       </div>
     </>
   )
